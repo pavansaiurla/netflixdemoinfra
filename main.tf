a66a840667455d2d08c826fc9a044a4183a79819
pavansaiurla <pavansaiurla476@gmail.com>
2024-12-10 22:10:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
 count = 5
 ami = "ami-0ddc798b3f1a5117e"
 instance_type = "t2.medium"
-key_name = "rahull"
+key_name = "my-terraform"
 vpc_security_group_ids = ["sg-05f044979e305302e"]
 tags = {
 Name = var.instance_names[count.index]
